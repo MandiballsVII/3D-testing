9ef225a05c0c2749fc7f701772c21593e67d076f
MandiballsVII <149199591+MandiballsVII@users.noreply.github.com>
2025-12-21 18:38:12 +0100

    Cinemachine camera y rotacion

--- Assets/GameDevTV Assets/RocketBoostScripts/Movement.cs
+++ Assets/GameDevTV Assets/RocketBoostScripts/Movement.cs
@@ -4,8 +4,10 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
     [SerializeField] int thrustStrength = 1000;
     Rigidbody rb;
+    [SerializeField] float rotationStrenght = 1.2f;
 
     private void Start()
     {
@@ -15,17 +17,38 @@ public class Movement : MonoBehaviour
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
     private void OnDisable()
     {
         thrust.Disable();
+        rotation.Disable();
     }
 
     void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
     {
         if (thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
+    }
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if(rotationInput < 0)
+            ApplyRotation(rotationStrenght);
+        else if(rotationInput > 0)
+            ApplyRotation(-rotationStrenght);
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
     }
 }
